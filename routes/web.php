e0d27ef9b906fc90eb8e8853027e0c15ffd52493
vincentvanp <52759139+vincentvanp@users.noreply.github.com>
2021-12-14 12:06:49 +0100

    Fixed querys

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,8 @@ Route::get('oauth/callback/{provider}', [oAuthController::class, 'handleProvider
 
 Route::view('/login-admin', 'login')->where('login', '.*');
 
-Route::view('/{any}', 'dashboard')->where('any', '.*');
-
+//Route::view('/{any}', 'dashboard')->where('any', '.*');
+Route::get('test', [StudentController::class, 'getAttendingStudents']);
 
 Route::group(['middleware' => ['auth']], function () {
     //only authorized users can access these routes

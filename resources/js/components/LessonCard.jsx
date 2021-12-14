bb51c90c4238d30787875f1440a53d830fdd42e2
rickert <rickert.goyvaerts@student.kdg.be>
2021-12-14 11:51:52 +0100

    card getting data and displaying it properly

--- resources/js/components/LessonCard.jsx
+++ resources/js/components/LessonCard.jsx
@@ -1,30 +1,51 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-
 import { Card, Button } from 'antd';
 
-function PreviousLessonCard (props){
+class PreviousLessonCard extends React.Component{
     
-    var studentsPresent = 20;
-    var totalStudents = 24;
+    state = {
+        present_students: [],
+        absent_students: []
+    };
 
-    return(
-        <Card className="card">
-            <div className="card--text">
-                <h3>{props.lesson.name}</h3>
-                <p className="card--text text--label">aanwezigheden:</p>
-                <p className="card--text card--text-atended">{studentsPresent}/{totalStudents} leerlingen</p>
-                <p className="card--text card--text-percentage">{Math.round(studentsPresent/totalStudents*100)}% was aanwezig</p>
-            </div>
-            <p className="text--lesson-time">{props.lesson.start}</p>
-            <div className="button-container--card">
-                <Button className="button--add-student" onClick={() => {
-                    window.location.href = "previous-lessons/" + props.lesson.id;
-                }}>Meer info</Button>
-            </div>
-        </Card>
-    );
+    async GetLessonData(){
+
+        const present_students = await axios.post('/get-attending-students', {lesson_id: this.props.lesson.id});
+        const absent_students = await axios.post('/get-absent-students', {lesson_id: this.props.lesson.id});
+
+        this.setState({ present_students: present_students.data,
+                        absent_students: absent_students.data});
+    }
+
+    componentDidMount(){
+        this.GetLessonData();
+    }
+
+    render(){
+
+        const {present_students, absent_students} = this.state;
+
+        var studentsPresent = present_students.length;
+        var totalStudents = present_students.length + absent_students.length;
+
+        return(
+            <Card className="card">
+                <div className="card--text">
+                    <h3>{this.props.lesson.name}</h3>
+                    <p className="card--text text--label">aanwezigheden:</p>
+                    <p className="card--text card--text-atended">{studentsPresent}/{totalStudents} leerlingen</p>
+                    <p className="card--text card--text-percentage">{Math.round(studentsPresent/totalStudents*100)}% was aanwezig</p>
+                </div>
+                <p className="text--lesson-time">{this.props.lesson.start}</p>
+                <div className="button-container--card">
+                    <Button className="button--add-student" onClick={() => {
+                        window.location.href = "previous-lessons/" + this.props.lesson.id;
+                    }}>Meer info</Button>
+                </div>
+            </Card>
+        );
+    }
 }
 
 export default PreviousLessonCard;
--- resources/js/views/PreviousLessonDetailView.jsx
+++ resources/js/views/PreviousLessonDetailView.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import PreviousDetail from "../components/previousDetail"
+import { Layout } from 'antd';
 
 function PreviousLessonDetailView(){
 
     return(
-        <div className="container--user-table">
-            <div className="container--active-table">
-                <PreviousDetail lesson_id={useParams()}/>
+        <Layout className="container--content">
+            <div className="container--text-title">
+                <h1 className="text--page-title">Meer informatie</h1>
             </div>
-        </div>
+            <div className="container--page">
+                <div className="container--user-table">
+                    <div className="container--active-table">
+                        <PreviousDetail lesson_id={useParams()}/>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     );
 
 }

eb98d755b90ae52e0577724e1b17e95a1b46a655
Rickert <rickert.goyvaerts@gmail.com>
2021-12-21 11:03:52 +0100

    adding data to charts

--- resources/js/components/Charts/BarChart.jsx
+++ resources/js/components/Charts/BarChart.jsx
@@ -3,25 +3,16 @@ import React from "react";
 import { Chart as ChartJS } from 'chart.js/auto'
 import { Bar } from 'react-chartjs-2'
 
-const BarChart = (dataSet) =>{
-
+const BarChart = (props) =>{
 
     return(
         <Bar 
             data={{
-                labels: dataSet.dataSet.labels,
+                labels: props.dataSet.labels,
                 datasets: [{
-                    label: '# of Votes',
-                    data: dataSet.dataSet.data,
+                    label: props.name,
+                    data: props.dataSet.data,
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
@@ -29,7 +20,6 @@ const BarChart = (dataSet) =>{
                         'rgba(153, 102, 255, 1)',
                         'rgba(255, 159, 64, 1)'
                     ],
-                    borderWidth: 1
                 }]
             }}
         />
--- resources/js/components/dashboardCharts.jsx
+++ resources/js/components/dashboardCharts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import BarChart from "../components/Charts/BarChart";
 import PieChart from "../components/Charts/PieChart";
 import LineChart from "./Charts/LineChart";
 
@@ -7,18 +8,48 @@ const axios = require('axios').default;
 
 class dashboardCharts extends React.Component {
     state = {
-        dataSet: {
+        dataSet1: {
+            data: "",
+            labels: ""
+        },
+        dataSet2: {
             data: "",
             labels: ""
         },
     }
 
     async getChartData(){
-        const res = await axios.get("https://api.coincap.io/v2/assets/?limit=5");
-        const datas = res.data.data;
+        
+        const {data} = await axios.post('/get-courses');
+        
+        var percentageArray = [];
+        var labels = [];
+        
+        for(let i = 0; i < data.length; i++){
+            const res = await axios.post("/get-course-attendence", {id: data[i].id});
 
-        this.setState({ dataSet: {data: datas.map((data) => (data.priceUsd)),
-                        labels: datas.map((data) => (data.name))}})    
+            labels.push(data[i].name);
+            percentageArray.push(res.data);
+        }
+
+        const lessons = await axios.post('/previous-lessons');
+
+        var percentageArray2 = [];
+        var labels2 = [];
+
+        for(let j = 0; j < lessons.data.lessons.length; j++){
+            const latePercentage = await axios.post('/get-late-percentage', {id: lessons.data.lessons[j].id});
+    
+            percentageArray2.push(latePercentage.data);
+            labels2.push(lessons.data.lessons[j].name);
+        }
+
+        this.setState({ dataSet1: {data: percentageArray,
+                                    labels: labels}});
+
+        this.setState({ dataSet2: {data: percentageArray2,
+                                    labels: labels2}})
+                                    
     }
 
     componentDidMount(){
@@ -27,15 +58,15 @@ class dashboardCharts extends React.Component {
 
     render(){
 
-        const {dataSet} = this.state;
-
-        if(dataSet.data == ""){
+        const {dataSet1, dataSet2} = this.state;
+        
+        if(dataSet1.data == ""){
             return <h1>test</h1>
         }else{
             return (
                 <div className="chart">
-                    <PieChart dataSet={dataSet}/>
-                    <LineChart dataSet={dataSet}/>
+                    <BarChart dataSet={dataSet1} name="percentage aanwezig per les"/>
+                    <BarChart dataSet={dataSet2} name="percentage te laat per les"/>
                 </div>
             );
         }

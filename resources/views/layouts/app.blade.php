28a6ed636d3ae73c36af10212cc9dbb0265027cd
Rickert <rickert.goyvaerts@gmail.com>
2021-12-25 10:33:50 +0100

    favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="apple-touch-icon" sizes="180x180" href="../images.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+        <link rel="manifest" href="../site.webmanifest">
+        <link rel="mask-icon" href="../safari-pinned-tab.svg" color="#201f1f">
+        <meta name="msapplication-TileColor" content="#201f1f">
+        <meta name="theme-color" content="#201f1f">
+
         <title>kaart scanner KDG</title>
 
         <!-- Fonts -->

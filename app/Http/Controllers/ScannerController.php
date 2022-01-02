2a6974f18d5fc2fc8a54bef5e9311e3f2671b9fb
vincentvanp <52759139+vincentvanp@users.noreply.github.com>
2022-01-02 14:31:57 +0100

    Update ScannerController.php

--- app/Http/Controllers/ScannerController.php
+++ app/Http/Controllers/ScannerController.php
@@ -15,14 +15,29 @@ use Auth;
 class ScannerController extends Controller
 {
     //Returns all scanners to frontend
-    public function getScanners(){
+    public function getScanners()
+    {
         return Scanner::all()->toJson();
     }
 
     //Function to store a scanned student from pusher in the database
     public function storeStudent(Request $request)
     {
-        $student = Student::where('serial_number', $request["serial"])->first();
+
+        if(isset($request["serial"]))
+        {
+            $student = Student::where('serial_number', $request["serial"])->first();
+        }
+        elseif(isset($request["name"]))
+        {
+            $student = Student::where('name', $request["name"])->first();
+        }
+        else
+        {
+            return ['status' => 'ERROR'];
+        }
+
+        
         $lesson = Lesson::where('id', $request["lesson_id"])->first();
         $user = Auth::user();
 
@@ -43,48 +58,8 @@ class ScannerController extends Controller
         
         $user->scancount += 1;
         $user->save();
-        
-        // return json_encode(array(
-            //     'name' => $student->name,
-            //     'serial' => $request["serial"],
-            //     'time' => $time,
-            // ));
             
         return ['status' => 'OK'];
     }
 
-    public function storeStudentByName(Request $request)
-    {
-        
-        $student = Student::where('name', $request["name"])->first();
-        $lesson = Lesson::where('id', $request["lesson_id"])->first();
-        $user = Auth::user();
-
-
-        $attendedLessons = $student->attendedLessons;
-
-        //Change present to true by checking if studenthaslesson already exists
-        if($student->lessons->contains($lesson)){
-            $student->lessons()->updateExistingPivot($lesson,['present' => 1]);
-        }
-
-        else{
-            $student->lessons()->attach($lesson,['present' => 1]);
-        }
-
-        $time = Carbon::parse($student->lessons()->first()->pivot->updated_at)->format('H:i');
-
-        event(new populateUserTable($student->name, $student->serial_number , $time));
-        
-        $user->scancount += 1;
-        $user->save();
-        
-        // return json_encode(array(
-            //     'name' => $student->name,
-            //     'serial' => $request["serial"],
-            //     'time' => $time,
-            // ));
-            
-        return ['status' => 'OK'];
-    }
 }
